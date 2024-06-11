a6aef7e4dbc478c9adb74d04655a92432173ca56
Paul Angelo Reyes <paulangeloreyes.official@gmail.com>
2024-06-12 00:44:40 +0800

    add access control

--- trinidad_kianlee/backend/rest.php
+++ trinidad_kianlee/backend/rest.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://trinidad-api/netlify.app/trinidad_kianlee/index.html");
 header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 
